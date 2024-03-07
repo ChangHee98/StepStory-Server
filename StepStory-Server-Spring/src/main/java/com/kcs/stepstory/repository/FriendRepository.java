3efc806e4ce083c68c9998e9b8953dfe4bcaa811
장동겸 <86873281+dongkyeomjang@users.noreply.github.com>
2024-03-07 16:33:54 +0900

    style: migrate springBoot

--- StepStory-Server-Spring/src/main/java/com/kcs/stepstory/repository/FriendRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kcs.stepstory.repository;
-
-import com.kcs.stepstory.domain.Friend;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FriendRepository extends JpaRepository<Friend, Long> {
-}
--- StepStory-Server-Spring/src/main/java/com/kcs/stepstory/security/info/JwtUserInfo.java
+++
@@ -1,6 +0,0 @@
-package com.kcs.stepstory.security.info;
-
-import com.kcs.stepstory.dto.type.ERole;
-
-public record JwtUserInfo(Long userId, ERole role) {
-}
