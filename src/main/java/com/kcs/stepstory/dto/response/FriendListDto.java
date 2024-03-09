559013041f07e0550043b922cf375e8a0f8f2d85
leeinhong95 <leeinhong9512@gmail.com>
2024-03-10 01:21:50 +0900

    feat: FriendListDto 기능추가 -

---
+++ src/main/java/com/kcs/stepstory/dto/response/FriendListDto.java
@@ -0,0 +1,15 @@
+package com.kcs.stepstory.dto.response;
+
+
+import lombok.*;
+
+import java.util.List;
+
+@Builder
+/**
+ *  FriendListDto recode는 FriendDto타입의 필드선언하여 FriendDto 객체의 리스트 형태로 저장한다.
+ */
+public record FriendListDto(
+        List<FriendDto> friendListDtos,
+        List<FriendDto> requestfriendListDtos
+){}
