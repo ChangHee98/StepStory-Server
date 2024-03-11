841a37ea3d930b366a67c76c294b39266d6f0525
leeinhong95 <leeinhong9512@gmail.com>
2024-03-11 20:23:51 +0900

    docs: gradlew, StepCountForAllDto 동기화

--- src/main/java/com/kcs/stepstory/dto/response/StepCountForAllDto.java
+++ src/main/java/com/kcs/stepstory/dto/response/StepCountForAllDto.java
@@ -3,20 +3,20 @@ package com.kcs.stepstory.dto.response;
 import lombok.Builder;
 @Builder
 public record StepCountForAllDto (
-    Integer seoul,
-    Integer busan,
-    Integer incheon,
-    Integer gwanju,
-    Integer ulsan,
-    Integer sejong,
-    Integer gyeonggi,
-    Integer gangwon,
-    Integer chungbuk,
-    Integer chungnam,
-    Integer jeonbuk,
-    Integer jeonnam,
-    Integer gyeongbuk,
-    Integer gyeongnam,
-    Integer jeju
+        Integer seoul,
+        Integer busan,
+        Integer incheon,
+        Integer gwanju,
+        Integer ulsan,
+        Integer sejong,
+        Integer gyeonggi,
+        Integer gangwon,
+        Integer chungbuk,
+        Integer chungnam,
+        Integer jeonbuk,
+        Integer jeonnam,
+        Integer gyeongbuk,
+        Integer gyeongnam,
+        Integer jeju
 ) {
 }
