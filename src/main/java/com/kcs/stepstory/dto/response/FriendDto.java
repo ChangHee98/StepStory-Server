6a56d934cfaa724e7099432ce84be7afa064f9b0
leeinhong95 <leeinhong9512@gmail.com>
2024-03-12 18:47:10 +0900

    refactor: FriendDto fromEntity 메서드 추가 친구 상세정보 확인 기능에 대한 fromEntity 추가

--- src/main/java/com/kcs/stepstory/dto/response/FriendDto.java
+++ src/main/java/com/kcs/stepstory/dto/response/FriendDto.java
@@ -3,6 +3,8 @@ package com.kcs.stepstory.dto.response;
 import com.kcs.stepstory.domain.User;
 import lombok.Builder;
 
+import java.util.Optional;
+
 
 /**
  * DTO 계층 구현
@@ -15,40 +17,32 @@ public record FriendDto (
     String selfIntro
 ){
 
-    /**
-     * 친구 목록 조회 OR 친구요청 목록조회 기능 친구 닉네임 검색 기능에 대한 DTO변환 메서드
-     * fromNicknameAndProfileImgUrl 메소드 -> nickname, profileImgUrl 만 조회하는 경우 수행
-     */
-    public static FriendDto fromNicknameAndProfileImgUrl(String nickname, String profileImgUrl){
-        return FriendDto.builder().
-                nickname(nickname).
-                profileImgUrl(profileImgUrl).build();
-    }
-
-    /**
-     * 친구 상세정보 기능에 대한 DTO 변환 메서드
-     */
-    public static FriendDto fromNicknameAndProfileImgUrlAndSelfIntro(String nickname, String profileImgUrl, String selfIntro){
-        return FriendDto.builder().
-                nickname(nickname).
-                profileImgUrl(profileImgUrl).
-                selfIntro(selfIntro).
-                build();
-    }
-
 
 
     /**
-     * USER 엔티티 타입받을 상황 대비해 확장한 메서드
+     * USER 엔티티 타입을 Dto 타입으로 변환 메서드
      * 만약 쿼리메서드가 User 엔티티 자체를 가지고 올 때,
      * 예시) select * from user where 특정 행 -> 즉 특정 모든 열값에 대한 하나의 행을 가지고 올때 사용되는 메서드
      */
     public static FriendDto fromEntity(User user){
+//        FriendDto friendDto = new FriendDto();
+//        friendDto.set
+
         return FriendDto.builder().
                 nickname(user.getNickname()).
-                profileImgUrl(user.getProfileImgUrl()).build();
+                profileImgUrl(user.getProfileImgUrl()).
+                build();
     }
 
+    public static FriendDto fromEntityDetails(User user){
+//        FriendDto friendDto = new FriendDto();
+//        friendDto.set
 
+        return FriendDto.builder().
+                nickname(user.getNickname()).
+                profileImgUrl(user.getProfileImgUrl()).
+                selfIntro(user.getSelfIntro()).
+                build();
+    }
 
 }
