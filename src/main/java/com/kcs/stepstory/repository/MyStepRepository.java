b1c8894e5d66f28399fc0a8e720307b27cd6c7ea
leeinhong95 <leeinhong9512@gmail.com>
2024-03-13 23:59:19 +0900

    feat: MyStepRepository 마이스토리 발자국 조회 - 기능 추가
    발자국 조회 - 전체

---
+++ src/main/java/com/kcs/stepstory/repository/MyStepRepository.java
@@ -0,0 +1,25 @@
+package com.kcs.stepstory.repository;
+
+import com.kcs.stepstory.domain.Step;
+import com.kcs.stepstory.domain.StepId;
+import com.kcs.stepstory.dto.response.FriendDto;
+import com.kcs.stepstory.dto.response.StepCountForAllDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MyStepRepository extends JpaRepository<Step, StepId> {
+
+
+
+
+    /**
+     *  발자국 조회 - 전체
+     *  특정 user에 대한 모든 객체 반환
+     */
+    @Query("SELECT s FROM Step s WHERE s.user.id = :userId")
+    List<Step> findByAllAreaListForMyStep(@Param("userId") Long userId);
+
+}
